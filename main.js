0be3f98cbb3899f13e46abde4ab0d4b1c226cf81
ste163 <samtedwards@pm.me>
2021-02-19 17:09:07 -0500

    added window state management

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules
 const {app, BrowserWindow} = require('electron')
+const windowStateKeeper = require('electron-window-state')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,16 +9,26 @@ let mainWindow
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
 
+  // Window state keeper --- for having the same window location & size based on when the user closed app
+  let winState = new windowStateKeeper({
+    defaultWidth: 500, defaultHeight: 650
+  })
+
   mainWindow = new BrowserWindow({
-    width: 1000, height: 800,
+    x: winState.x, y: winState.y,
+    width: winState.width, height: winState.height,
+    minWidth: 350, maxWidth: 650, minHeight: 300,
     webPreferences: { nodeIntegration: true }
   })
 
   // Load main.html into the new BrowserWindow
   mainWindow.loadFile('renderer/main.html')
 
+  // Tell the windowStateKeeper which window it should manager
+  winState.manage(mainWindow)
+
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools()
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
