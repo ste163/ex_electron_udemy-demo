77bbc013f978db9d5b341fc1f3b16a67a48073db
ste163 <samtedwards@pm.me>
2021-02-19 18:39:07 -0500

    can input data, recieve in main process, and send success back to renderer

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 // Modules
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const windowStateKeeper = require('electron-window-state')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+// Listen for new item request in global scope
+ipcMain.on('new-item', (e, itemUrl) => {
+  console.log(itemUrl)
+  // Get the new item and send back to the renderer
+  setTimeout(() => {
+    // e.sender allows you to gain access to which renderer sent the message
+    e.sender.send('new-item-success', 'New item from main process')
+  }, 2000)
+})
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
--- renderer/app.js
+++ renderer/app.js
@@ -1,3 +1,74 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+// Handles everything with the UI
+
+// Modules
+const { ipcRenderer } = require('electron')
+
+// DOM nodes
+const showModal = document.getElementById('show-modal'),
+      closeModal = document.getElementById('close-modal'),
+      modal = document.getElementById('modal'),
+      addItem = document.getElementById('add-item')
+      itemUrl = document.getElementById('url')
+
+
+// Disable and Enable modal buttons while submitting
+const toggleModalBtns = () => {
+    // Check btn state
+    if (addItem.disabled === true) {
+        // Revert to default state
+        addItem.disabled = false
+        addItem.style.opacity = 1
+        addItem.innerText = 'Add Item'
+        closeModal.style.display = 'inline'
+    } else {
+        // Set to disabled state
+        addItem.disabled = true
+        addItem.style.opacity = 0.5
+        addItem.innerText = 'Adding...'
+        closeModal.style.display = 'none'
+    }
+}
+
+// Listeners from Main Process
+// When we get an item back from the Main Process
+ipcRenderer.on('new-item-success', (e, newItem) => {
+    console.log(newItem)
+    toggleModalBtns()
+
+    // Hide modal and clear value
+    modal.style.display = 'none'
+    itemUrl.value = ''
+})
+
+// Show modal
+showModal.addEventListener('click', e => {
+    modal.style.display = 'flex'
+    // When modal opens, allow user to automatically type into field
+    itemUrl.focus()
+})
+
+// Hide modal
+closeModal.addEventListener('click', e => {
+    modal.style.display = 'none'
+})
+
+// Handle new item
+addItem.addEventListener('click', e => {
+    const url = itemUrl.value
+    // Check if url exists
+    if (url) {
+        // Send new item to main process to handle screen-grab
+        ipcRenderer.send('new-item', url)
+
+        // Disable btns
+        toggleModalBtns()
+    }
+})
+
+// Listen for keyboard submit on Enter key up
+itemUrl.addEventListener('keyup', e => {
+    // Check for Enter key
+    if (e.key === 'Enter') {
+        addItem.click()
+    }
+})
